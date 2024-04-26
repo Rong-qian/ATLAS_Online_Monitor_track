35bda70b46e4bd92d322407a0f02e9dd8fd2f878
Robert Myers <romyers@umich.edu>
2024-04-26 17:22:04 -0400

    Partial result for event display. TODO: Fix vertical line problem. TODO: Allow RTParam to compute itself from realtime data.

--- ROOT_plot/DAQMonitor/DataModel/DAQData.h
+++ ROOT_plot/DAQMonitor/DataModel/DAQData.h
@@ -37,6 +37,8 @@ struct DAQData {
     // Excludes empty events.
     std::vector<MuonReco::Event> newEvents;
 
+    std::vector<MuonReco::Event> eventDisplayBuffer;
+
     // A struct of histograms containing aggregate event data. See
     // src/DataModel/Plots.cpp.
     Plots plots;
@@ -70,6 +72,9 @@ struct DAQData {
     void lock  () const; // Locks an internal mutex
     void unlock() const; // Unlocks an internal mutex
 
+    void binEvent     (MuonReco::Event &e  );
+    void updateHitRate(int total_events    );
+
     /**
      * Clears all data stored in the DAQData. Clear is called at the
      * beginning of each run to clear any latent data from the last run.
@@ -88,5 +93,8 @@ private:
     DAQData();
 
     mutable std::mutex dataLock;
+
+    std::vector<std::vector<double>> nHits ;
+    std::vector<std::vector<double>> nTotal;
 
 };
